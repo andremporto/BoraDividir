4bab81dbb59722a7e6d7f5f81e82d7f897fb78eb
André Porto <andreporto@me.com>
2023-06-30 08:21:35 -0300

    Initial Commit

---
+++ BoraDividir/BoraDividirApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoraDividirApp.swift
+//  BoraDividir
+//
+//  Created by André Porto on 30/06/23.
+//
+
+import SwiftUI
+
+@main
+struct BoraDividirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoraDividir/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BoraDividir
+//
+//  Created by André Porto on 30/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
